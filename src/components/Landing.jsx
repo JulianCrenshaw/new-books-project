806553b8e5be8b0896c4a434025596f6f5749f91
Julian <julian.gold.crenshaw@gmail.com>
2026-02-12 06:53:24 -0600

    landing

---
+++ src/components/Landing.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Landing = () => {
+  return (
+<section id='landing'>
+<header>
+    <div className="header__container">
+        <div className="header__description">
+            <h1>America's most awarded online library platform</h1>
+            <h2>Find your dream book with <span className="purple"></span></h2>
+        </div>
+    </div>
+</header>
+</section>
+  )
+}
+
+export default Landing
