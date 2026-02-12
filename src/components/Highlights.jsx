b955af72c01b57c567fb90379e643db933b7f360
Julian <julian.gold.crenshaw@gmail.com>
2026-02-12 07:24:54 -0600

    highlights

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -23,18 +23,18 @@ const Highlights = () => {
               <div className="highlight__img">
                 <FontAwesomeIcon icon="book-open" />
               </div>
-              <h3 className="highlight__subtitle"> Easy and Quick</h3>
+              <h3 className="highlight__subtitle"> 10,000+ Books</h3>
               <p className="highlight__para">
-                Get access to the book you purchased online instantly
+                Library has books in all your favorite catagories
               </p>
             </div>
             <div className="highlight">
               <div className="highlight__img">
                 <FontAwesomeIcon icon="tags" />
               </div>
-              <h3 className="highlight__subtitle"> Easy and Quick</h3>
+              <h3 className="highlight__subtitle"> Affordable</h3>
               <p className="highlight__para">
-                Get access to the book you purchased online instantly
+                Get your hands on popular books for as little as $10
               </p>
             </div>
           </div>
