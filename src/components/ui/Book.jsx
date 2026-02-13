ecfd23f7457ca1f14fd0029cb039c2b3a092b23f
Julian <julian.gold.crenshaw@gmail.com>
2026-02-13 08:05:49 -0600

    features

--- src/components/ui/Book.jsx
+++ src/components/ui/Book.jsx
@@ -2,11 +2,12 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 
 const book = ({ book }) => {
+  
   return (
       <div className="book">
         <a href="">
           <figure className="book__img--wrapper">
-            <img src="book.url" alt="" className="book__img" />
+            <img src={book.url} alt="" className="book__img" />
           </figure>
         </a>
         <div className="book__atitle">
@@ -15,20 +16,18 @@ const book = ({ book }) => {
           </a>
         </div>
         <div className="book__ratings">
-          <FontAwesomeIcon icon="star" />
-          <FontAwesomeIcon icon="star" />
-          <FontAwesomeIcon icon="star" />
-          <FontAwesomeIcon icon="star" />
-          <FontAwesomeIcon icon="star-half-alt" />
+         {
+          new Array(5).fill(0).map((_, index) => <FontAwesomeIcon icon="star" key={index} />)
+         }
         </div>
         <div className="book__price">
           {book.salePrice ? (
             <>
-              <span className="book__price--normal">${book.originalPrice}</span>
-              ${book.salePrice}
+              <span className="book__price--normal">${book.originalPrice.toFixed(2)}</span>
+              ${book.salePrice.toFixed(2)}
             </>
           ) : (
-            "null"
+            <> ${book.originalPrice.toFixed(2)}</>
           )}
         </div>
       </div>
