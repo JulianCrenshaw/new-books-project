c3ab65fd1cbf8dc958a7edca21311d8410e5ea01
Julian <julian.gold.crenshaw@gmail.com>
2026-02-13 07:17:10 -0600

    data

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,6 +1,10 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React from 'react'
+import Book from './ui/Book'
+import { books } from '../data'
 
 const Features = () => {
+  console.log( books )
   return (
 <section id="features">
   <div className="container">
@@ -8,6 +12,12 @@ const Features = () => {
       <h2 className="section__title">
         Features <span className="blue">Books</span>
       </h2>
+      <div className="books">
+       <Book />
+       <Book />
+       <Book />
+       <Book />
+      </div>
     </div>
   </div>
 </section>
