2d966e041b9015a2e5e93744e438a8c1abdfebc6
Julian <julian.gold.crenshaw@gmail.com>
2026-02-16 07:31:55 -0600

    key

--- src/components/Discounted.jsx
+++ src/components/Discounted.jsx
@@ -1,22 +1,24 @@
-import React from 'react'
-import { books } from '../data'
-import Book from './ui/Book'
+import React from "react";
+import { books } from "../data";
+import Book from "./ui/Book";
 
 const Discounted = () => {
   return (
-<section id='recent'>
-<div className="container">
-    <div className="row">
-        <div className="section__title">
+    <section id="recent">
+      <div className="container">
+        <div className="row">
+          <div className="section__title">
             Discounted <span className="blue"></span>
+          </div>
+          <div className="books">
+            {books.map((book) => (
+              <Book book={book} key={book.id} />
+            ))}
+          </div>
         </div>
-        <div className="books">
-        {books.map(book => <Book book={book} />)}
-        </div>
-    </div>
-</div>
-</section>
-  )
-}
+      </div>
+    </section>
+  );
+};
 
-export default Discounted
+export default Discounted;
