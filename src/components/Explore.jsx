b286ce73d5af6253550d525e2891b82e4559aa19
Julian <julian.gold.crenshaw@gmail.com>
2026-02-16 07:45:38 -0600

    explore

---
+++ src/components/Explore.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Explore = () => {
+  return (
+   <secrion id="explor">
+    <div className="container">
+        <div className="row row__column">
+            <h2 className="section__title">
+                Explore more <span className="blue">Books</span>
+            </h2>
+        </div>
+    </div>
+   </secrion>
+  )
+}
+
+export default Explore
