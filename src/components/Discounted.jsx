0915d8a9f8811484f29c4280e26d405973f0a445
Julian <julian.gold.crenshaw@gmail.com>
2026-02-16 07:25:56 -0600

    discounted

--- src/components/Discounted.jsx
+++ src/components/Discounted.jsx
@@ -3,7 +3,16 @@ import React from 'react'
 const Discounted = () => {
   return (
 <section id='recent'>
-
+<div className="container">
+    <div className="row">
+        <div className="section__title">
+            Discounted <span className="blue"></span>
+        </div>
+        <div className="books">
+            
+        </div>
+    </div>
+</div>
 </section>
   )
 }
