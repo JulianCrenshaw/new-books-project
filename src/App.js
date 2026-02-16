86764be5e03be0e1ed4ba2cc99dfe16721577d08
Julian <julian.gold.crenshaw@gmail.com>
2026-02-16 07:57:51 -0600

    footer

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Highlights from './components/Highlights'
 import Features from './components/Features';
 import Discounted from './components/Discounted';
 import Explore from './components/Explore';
+import Footer from './components/Footer';
 
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
       <Features />
       <Discounted />
       <Explore />
+      <Footer />
     </div>
   );
 }
