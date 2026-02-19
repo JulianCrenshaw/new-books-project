ca3d3ae114d8eecb1f011abcd80dd5d49b2d0166
Julian <julian.gold.crenshaw@gmail.com>
2026-02-19 07:09:40 -0600

    components

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Nav from './components/Nav';
 import Footer from './components/Footer';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 import Home from './pages/Home';
+import Books from './pages/Books';
 
 
 function App() {
@@ -9,8 +10,8 @@ function App() {
     <Router>
     <div className="App">
       <Nav />
-      <Route path="/" exact component={Home}/>
-
+      <Route path="/" exact component={Home} />
+      <Route path="/books" component={Books} />
       <Footer />
     </div>
     </Router>
