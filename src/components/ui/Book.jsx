5845c98dd75c710e7df17dd0872541ce1300ff26
Julian <julian.gold.crenshaw@gmail.com>
2026-02-20 07:18:44 -0600

    price

--- src/components/ui/Book.jsx
+++ src/components/ui/Book.jsx
@@ -2,33 +2,24 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 import { Link } from "react-router-dom/cjs/react-router-dom";
 import Rating from "./Rating";
+import Price from " ./Price"
 
 const book = ({ book }) => {
-  
   return (
-      <div className="book">
-        <Link to="/books/1">
-          <figure className="book__img--wrapper">
-            <img src={book.url} alt="" className="book__img" />
-          </figure>
+    <div className="book">
+      <Link to="/books/1">
+        <figure className="book__img--wrapper">
+          <img src={book.url} alt="" className="book__img" />
+        </figure>
+      </Link>
+      <div className="book__title">
+        <Link to="/books/1" className="book__title--link">
+          {book.title}
         </Link>
-        <div className="book__title">
-          <Link to="/books/1" className="book__title--link">
-            {book.title}
-          </Link>
-        </div>
-        <Rating rating={book.rating} />
-        <div className="book__price">
-          {book.salePrice ? (
-            <>
-              <span className="book__price--normal">${book.originalPrice.toFixed(2)}</span>
-              ${book.salePrice.toFixed(2)}
-            </>
-          ) : (
-            <> ${book.originalPrice.toFixed(2)}</>
-          )}
-        </div>
       </div>
+      <Rating rating={book.rating} />
+      <Price salePrice={book.salePrice} originalPrice={originalPrice} />
+    </div>
   );
 };
 
