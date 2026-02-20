6c8f7e368bf60822a3cdf55de0b0ffb0ddec8781
Julian <julian.gold.crenshaw@gmail.com>
2026-02-20 06:42:54 -0600

    arrow

--- src/index.js
+++ src/index.js
@@ -12,10 +12,11 @@ import {
   faBookOpen,
   faTags,
   faStar,
-  faStarHalfAlt
+  faStarHalfAlt,
+  faArrowLeft
 } from "@fortawesome/free-solid-svg-icons";
 
-library.add(faBars, faShoppingCart, faTimes, faBolt, faBookOpen, faTags, faStar, faStarHalfAlt);
+library.add(faBars, faShoppingCart, faTimes, faBolt, faBookOpen, faTags, faStar, faStarHalfAlt, faArrowLeft);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
