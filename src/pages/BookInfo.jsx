c6bf2733f7c66e6bdfcf5b8a76c396a482ba2e29
Julian <julian.gold.crenshaw@gmail.com>
2026-02-20 06:49:47 -0600

    imags

--- src/pages/BookInfo.jsx
+++ src/pages/BookInfo.jsx
@@ -1,23 +1,31 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import React from 'react'
-import { Link } from 'react-router-dom/cjs/react-router-dom'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from "react";
+import { Link } from "react-router-dom/cjs/react-router-dom";
 
 const BookInfo = ({ books }) => {
-    return (
- <div id="books__body">
-    <main id="books__main">
+  return (
+    <div id="books__body">
+      <main id="books__main">
         <div className="books__container">
-            <div className="row">
-                <div className="book__selected--top">
-                    <Link to="/books" className="book__link">
-                    <FontAwesomeIcon icon="arrow-left" />
-                    </Link>
-                </div>
+          <div className="row">
+            <div className="book__selected--top">
+              <Link to="/books" className="book__link">
+                <FontAwesomeIcon icon="arrow-left" />
+              </Link>
+              <Link to="/books" className="book__link">
+                <h2 className="book__selected--title--top">Books</h2>
+              </Link>
             </div>
+            <div className="book__selected">
+                <figure className="book__selected--figure">
+                    <img src="" alt="" className="book__selected--img" />
+                </figure>
+            </div>
+          </div>
         </div>
-    </main>
- </div>
-  )
-}
+      </main>
+    </div>
+  );
+};
 
-export default BookInfo
+export default BookInfo;
