a59f03b9950c1731e2086b7b1c20b64498a3d31e
Julian <julian.gold.crenshaw@gmail.com>
2026-02-20 06:45:39 -0600

    book1

--- src/components/ui/Book.jsx
+++ src/components/ui/Book.jsx
@@ -6,13 +6,13 @@ const book = ({ book }) => {
   
   return (
       <div className="book">
-        <Link to="">
+        <Link to="/books/1">
           <figure className="book__img--wrapper">
             <img src={book.url} alt="" className="book__img" />
           </figure>
         </Link>
         <div className="book__title">
-          <Link to="/" className="book__title--link">
+          <Link to="/books/1" className="book__title--link">
             {book.title}
           </Link>
         </div>
