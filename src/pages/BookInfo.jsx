e9f6f8e1ba4edff01c74af13a725c4710afeb8d3
Julian <julian.gold.crenshaw@gmail.com>
2026-02-24 07:09:33 -0600

    para

--- src/pages/BookInfo.jsx
+++ src/pages/BookInfo.jsx
@@ -28,7 +28,14 @@ const BookInfo = ({ books }) => {
                     <div className="book__selected--price">
                     <Price originalPrice={50} salePrice={20} />
                     </div>
-                    <div className="book__summary">An amazing book about a great deal of practice in how to achieve success in a interview for a coding job near your location weather if it is long term or short term. More books like this are availabe below!</div>
+                    <div className="book__summary">
+                      <div className="book__summary--title">
+                        Summary
+                      </div>
+                      <p className="book__summary--para">
+                        An amazing book about a great deal of practice in how to achieve success in a interview for a coding job near your location weather if it is long term or short term. More books like this are availabe below!
+                      </p>
+                    </div>
                 </div>
             </div>
           </div>
