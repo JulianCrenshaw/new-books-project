48d24ee5c2fb646dcbb1bf23515f15182b47f12c
Julian <julian.gold.crenshaw@gmail.com>
2026-02-27 07:44:39 -0600

    bookInfo

--- src/pages/BookInfo.jsx
+++ src/pages/BookInfo.jsx
@@ -7,7 +7,7 @@ import Book from "../components/ui/Book";
 
 const BookInfo = ({ books, addToCart, cart }) => {
   const { id } = useParams();
-  const book = books.find((book) => +book.id === +id); 
+  const book = books.find((book) => +book.id === +id);
 
   function addBookToCart(book) {
     addToCart(book);
