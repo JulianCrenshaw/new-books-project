ea40c1f6a66792aa940454bcbf568c35c327c60f
Julian <julian.gold.crenshaw@gmail.com>
2026-04-06 20:50:42 -0500

    landing changes

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -10,9 +10,17 @@ const Landing = () => {
         <div className="header__description">
             <h1>America's most awarded online library platform</h1>
             <h2>Find your dream book with <span className="blue">Library</span></h2>
-            <a href="#features">
-                <button className="btn">Browse Books</button>
-            </a>
+            <button
+            className="btn"
+            onClick={() => {
+                const section =
+                document.getElementById("features");
+                section?.scrollIntoView({ behavior:
+                    "smooth" });
+            }}
+            >Browse Books
+                
+            </button>
         </div>
         <figure className="header__img--wrapper">
             <img src={ UndrawBooks } alt="" />
